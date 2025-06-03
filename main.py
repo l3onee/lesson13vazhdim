503644d2c5185ee01fc8606a4673820bb7f803eb
l3onee <leonberatjashari@gmail.com>
2025-06-03 20:34:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-import numpy as py
+import pandas as pd
+products = ['apple', 'banana', 'orange', 'grape']
+sales = [90,100,110,120]
+
+sales_index = pd.Series(sales,index=products)
+print(sales_index)
+
+sales_series = pd.Series(sales,index=products)
+print(sales_series)
+
+print(sales_series['grape'])
+
+total_sales = sales_series.sum()
+print(total_sales)
+
+# data={
+#     'Name': ['Leon','Darsej','Qamil'],
+#     'Age' : [25,2,88],
+#     'City' : ['Ferizaj','Gjakove','New Jesey']
+# }
+
+# df = pd.DataFrame(data)
+# print(df)
+
+# df = pd.DataFrame(pd)
+# print(df)
+
+# df = pd.read_csv('cs.csv')
+# df.to_csv('leon.csv', index= False)
+
+best_selling_product = total_sales.idxmax()
+print(f"Best selling product is: {best_selling_product}")
