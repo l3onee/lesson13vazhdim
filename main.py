74b9c4edf058386947735b251f8a5066ca2f5952
l3onee <leonberatjashari@gmail.com>
2025-06-05 20:43:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,48 @@
 import pandas as pd
-products = ['apple', 'banana', 'orange', 'grape']
-sales = [90,100,110,120]
+data = {
+    "Germany,France,Portugal"
+}
 
-sales_index = pd.Series(sales,index=products)
-print(sales_index)
+df =pd.read_csv('avgIQpercountry.csv') 
+print(df.info())
 
-sales_series = pd.Series(sales,index=products)
-print(sales_series)
+print_rows = df.head() 
 
-print(sales_series['grape'])
+subset = df[['Country','Average IQ']]
+print(subset)
 
-total_sales = sales_series.sum()
-print(total_sales)
+filtered_df = df[['Average IQ'] < 100]
+print(filtered_df)
 
-# data={
-#     'Name': ['Leon','Darsej','Qamil'],
-#     'Age' : [25,2,88],
-#     'City' : ['Ferizaj','Gjakove','New Jesey']
-# }
 
-# df = pd.DataFrame(data)
-# print(df)
+# products = ['apple', 'banana', 'orange', 'grape']
+# sales = [90,100,110,120]
 
-# df = pd.DataFrame(pd)
-# print(df)
+# sales_index = pd.Series(sales,index=products)
+# print(sales_index)
 
-# df = pd.read_csv('cs.csv')
-# df.to_csv('leon.csv', index= False)
+# sales_series = pd.Series(sales,index=products)
+# print(sales_series)
 
-best_selling_product = total_sales.idxmax()
-print(f"Best selling product is: {best_selling_product}")
+# print(sales_series['grape'])
+
+# total_sales = sales_series.sum()
+# print(total_sales)
+
+# # data={
+
+# # df = pd.DataFrame(data)
+# #     'Name': ['Leon','Darsej','Qamil'],
+# #     'Age' : [25,2,88],
+# #     'City' : ['Ferizaj','Gjakove','New Jesey']
+# # }
+# # print(df)
+
+# # df = pd.DataFrame(pd)
+# # print(df)
+
+# # df = pd.read_csv('cs.csv')
+# # df.to_csv('leon.csv', index= False)
+
+# best_selling_product = total_sales.idxmax()
+# print(f"Best selling product is: {best_selling_product}")
